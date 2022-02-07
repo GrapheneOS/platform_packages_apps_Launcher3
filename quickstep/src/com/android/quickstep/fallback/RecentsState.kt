2cbad1a507eff054ae7559be903567d1c55a5a58
LibXZR <i@xzr.moe>
2022-02-07 10:59:50 +0800

    Launcher3: Introduce memory info in overview
    This is convenient for monitoring device status and many OEMs have their design for this. It's time to have our own one.
    
    Change-Id: I890c86c798055b1964506d9a4df1cef386562160
    Signed-off-by: LibXZR <i@xzr.moe>
    iusmac: adapt to A13.
    Signed-off-by: iusmac <iusico.maxim@libero.it>
    [neobuddy89: adapt to A13 QPR2, A14 QPR2, A15, A16]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/fallback/RecentsState.kt
+++ quickstep/src/com/android/quickstep/fallback/RecentsState.kt
@@ -80,6 +80,9 @@ open class RecentsState(@JvmField val ordinal: Int, private val mFlags: Int) :
     /** For this state, whether overview actions should be shown. */
     fun hasOverviewActions() = hasFlag(FLAG_OVERVIEW_ACTIONS)
 
+    /** For this state, whether mem info view should be shown. */
+    fun hasMemInfoView() = hasFlag(FLAG_MEMINFO)
+
     /** For this state, whether live tile should be shown. */
     fun hasLiveTile() = hasFlag(FLAG_LIVE_TILE)
 
@@ -252,6 +255,7 @@ open class RecentsState(@JvmField val ordinal: Int, private val mFlags: Int) :
         private val FLAG_TASK_THUMBNAIL_SPLASH = BaseState.getFlag(8)
         private val FLAG_ADD_DESK_BUTTON = BaseState.getFlag(9)
         private val FLAG_SHOW_EXPLODED_DESKTOP_VIEW = BaseState.getFlag(10)
+        private val FLAG_MEMINFO = BaseState.getFlag(11)
 
         private const val PREDICTIVE_BACK_DURATION = 1000L
         private const val PREDICTIVE_BACK_MAX_RECENTS_SCALE_LAUNCH = 1.1f
@@ -280,7 +284,8 @@ open class RecentsState(@JvmField val ordinal: Int, private val mFlags: Int) :
                     FLAG_LIVE_TILE or
                     FLAG_RECENTS_VIEW_VISIBLE or
                     FLAG_ADD_DESK_BUTTON or
-                    FLAG_SHOW_EXPLODED_DESKTOP_VIEW),
+                    FLAG_SHOW_EXPLODED_DESKTOP_VIEW or
+                    FLAG_MEMINFO),
             )
         @JvmField
         val MODAL_TASK: RecentsState =
@@ -293,7 +298,8 @@ open class RecentsState(@JvmField val ordinal: Int, private val mFlags: Int) :
                     FLAG_SCRIM or
                     FLAG_LIVE_TILE or
                     FLAG_RECENTS_VIEW_VISIBLE or
-                    FLAG_SHOW_EXPLODED_DESKTOP_VIEW),
+                    FLAG_SHOW_EXPLODED_DESKTOP_VIEW or
+                    FLAG_MEMINFO),
             )
         @JvmField
         val BACKGROUND_APP: RecentsState =
