7772cf67270bb790dc77ac1af92ad47a8fdf0159
Pranav Vashi <neobuddy89@gmail.com>
2022-06-05 13:14:15 +0530

    Launcher3: Make drawer cells square when labels are hidden
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -160,6 +160,7 @@ public class DeviceProfile {
     public int allAppsLeftRightMargin;
     public final int numShownAllAppsColumns;
     private float allAppsCellHeightMultiplier;
+    private boolean allAppsIconText;
 
     private final OverviewProfile overviewProfile;
 
@@ -315,6 +316,7 @@ public class DeviceProfile {
 
         allAppsCellHeightMultiplier =
                     (float) LauncherPrefs.ROW_HEIGHT.get(context) / 100F;
+        allAppsIconText = LauncherPrefs.SHOW_DRAWER_LABELS.get(context);
 
         setupAllAppsStyle(context);
 
@@ -865,7 +867,26 @@ public class DeviceProfile {
             hideWorkspaceLabelsIfNotEnoughSpace();
         }
 
+        // If drawer labels are disabled, make the All Apps cell closer to a square
+        // by basing its height on the drawer width and column count.
+        if (!allAppsIconText) {
+            int cellLayoutHorizontalPadding =
+                (mWorkspaceProfile.getCellLayoutPaddingPx().left
+                        + mWorkspaceProfile.getCellLayoutPaddingPx().right) / 2;
+            int leftRightPadding =
+                    getWorkspaceIconProfile().getDesiredWorkspaceHorizontalMarginPx();
+            int drawerWidth =
+                    mDeviceProperties.getAvailableWidthPx() - leftRightPadding * 2;
+
+            // Use the number of shown All Apps columns for actual cell width
+            int cellWidth = drawerWidth / numShownAllAppsColumns;
+            int cellHeight = (int) (cellWidth * allAppsCellHeightMultiplier);
+
+            mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(cellHeight);
+        }
+
         if (LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE.get(context)
+                && allAppsIconText
                 && !(mIsResponsiveGrid && getAllAppsProfile().getMaxAllAppsTextLineCount() == 2)) {
             // Add extra textHeight to the existing allAppsCellHeight.
             mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(
