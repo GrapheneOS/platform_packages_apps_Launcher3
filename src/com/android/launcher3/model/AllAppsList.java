c2b0ff716c01a1ce80b8e2d6627c201fa4f556d7
elpaablo <donopwhile1@gmail.com>
2022-10-27 03:32:27 +0100

    Launcher3: Switch to AppLock API for protected and hidden apps
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sat, 4 Mar 2023 22:08:50 +0530
        Subject: Launcher3: Update AppLock API and integrate hidden apps
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I519dafb9b9e8df683df532dcaf35c29acb7306e5
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/model/AllAppsList.java
+++ src/com/android/launcher3/model/AllAppsList.java
@@ -36,7 +36,7 @@ import com.android.launcher3.compat.AlphabeticIndexCompat;
 import com.android.launcher3.dagger.ApplicationContext;
 import com.android.launcher3.dagger.LauncherAppSingleton;
 import com.android.launcher3.icons.IconCache;
-import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
+import com.android.launcher3.lineage.trust.AppLockHelper;
 import com.android.launcher3.model.data.AppInfo;
 import com.android.launcher3.model.data.AppsListData;
 import com.android.launcher3.model.data.ItemInfo;
@@ -84,7 +84,7 @@ public class AllAppsList {
     @NonNull private final Provider<AppsListRepository> mRepo;
 
     private boolean mDataChanged = false;
-    private TrustDatabaseHelper mTrustData;
+    private AppLockHelper mAppLockHelper;
 
     private AlphabeticIndexCompat mIndex;
 
@@ -108,7 +108,7 @@ public class AllAppsList {
         mIconCache = iconCache;
         mAppFilter = appFilter;
         mRepo = repositoryProvider;
-        mTrustData = TrustDatabaseHelper.getInstance(context);
+        mAppLockHelper = AppLockHelper.getInstance(context);
         mIndex = new AlphabeticIndexCompat(LocaleList.getDefault());
     }
 
@@ -160,7 +160,7 @@ public class AllAppsList {
     }
 
     public void add(AppInfo info, LauncherActivityInfo activityInfo, boolean loadIcon) {
-        if (mTrustData != null && mTrustData.isPackageHidden(info.getTargetPackage())) {
+        if (mAppLockHelper != null && mAppLockHelper.isPackageHidden(info.getTargetPackage())) {
             return;
         }
         if (!mAppFilter.shouldShowApp(info.componentName)) {
