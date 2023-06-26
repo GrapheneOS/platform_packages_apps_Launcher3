0f635dbc7189bf3a4433ffb2a13bfb3d51c7c09c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-26 10:13:55 +0300

    add Storage Scopes link to launcher icon shortcuts

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -49,6 +49,7 @@ import static com.android.launcher3.popup.SystemShortcut.BUBBLE_SHORTCUT;
 import static com.android.launcher3.popup.SystemShortcut.DONT_SUGGEST_APP;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.PRIVATE_PROFILE_INSTALL;
+import static com.android.launcher3.popup.SystemShortcut.STORAGE_SCOPES;
 import static com.android.launcher3.popup.SystemShortcut.UNINSTALL_APP;
 import static com.android.launcher3.popup.SystemShortcut.WIDGETS;
 import static com.android.launcher3.taskbar.LauncherTaskbarUIController.ALL_APPS_PAGE_PROGRESS_INDEX;
@@ -479,6 +480,7 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
 
         shortcuts.addAll(getSplitShortcuts());
         shortcuts.add(WIDGETS);
+        shortcuts.add(STORAGE_SCOPES);
         shortcuts.add(INSTALL);
         if (Flags.enablePrivateSpaceInstallShortcut()) {
             shortcuts.add(PRIVATE_PROFILE_INSTALL);
