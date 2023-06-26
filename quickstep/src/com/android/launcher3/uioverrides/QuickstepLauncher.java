fba7ae20c18489f8083eee64d34fb1086425be62
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-26 10:13:55 +0300

    add Storage Scopes link to launcher icon shortcuts

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -60,6 +60,7 @@ import static com.android.launcher3.popup.SystemShortcut.DONT_SUGGEST_APP;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.PRIVATE_PROFILE_INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.REMOVE;
+import static com.android.launcher3.popup.SystemShortcut.STORAGE_SCOPES;
 import static com.android.launcher3.popup.SystemShortcut.UNINSTALL_APP;
 import static com.android.launcher3.popup.SystemShortcut.WIDGETS;
 import static com.android.launcher3.taskbar.LauncherTaskbarUIController.ALL_APPS_PAGE_PROGRESS_INDEX;
@@ -547,6 +548,7 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
 
         shortcuts.addAll(getSplitShortcuts());
         shortcuts.add(WIDGETS);
+        shortcuts.add(STORAGE_SCOPES);
         shortcuts.add(INSTALL);
         // TODO(b/444744861): Update private space apps to have its own container.
         boolean isPinnable = itemInfo instanceof ItemInfoWithIcon info
