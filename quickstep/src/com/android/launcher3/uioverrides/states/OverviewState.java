335573dfc60db051f1dabc201b26ab28da8eb0b1
nift4 <nift4@protonmail.com>
2023-08-23 15:58:29 +0200

    Launcher3: Show AOSP clear all button when chip disabled
    Change-Id: I96970593301a7577f2d71c908551b8ee06f43f0a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/OverviewState.java
@@ -120,6 +120,7 @@ public class OverviewState extends LauncherState {
     int elements = OVERVIEW_ACTIONS | MEMINFO | ADD_DESK_BUTTON;
         boolean showFloatingSearch;
         DeviceProfile dp = launcherUiState.getDeviceProfileRef().getValue();
+        Context context = dp.getDisplayInfo().context;
         if (dp.getDeviceProperties().isPhone()) {
             // Only show search in phone overview in portrait mode.
             showFloatingSearch = !dp.getDeviceProperties().isLandscape();
@@ -130,6 +131,11 @@ public class OverviewState extends LauncherState {
         if (showFloatingSearch) {
             elements |= FLOATING_SEARCH_BAR;
         }
+
+        if (!LauncherPrefs.RECENTS_CLEAR_ALL.get(context)) {
+            elements |= CLEAR_ALL_BUTTON;
+        }
+
         if (launcherUiState.isSplitSelectActiveRef().getValue()) {
             elements &= ~CLEAR_ALL_BUTTON & ~ADD_DESK_BUTTON;
         }
