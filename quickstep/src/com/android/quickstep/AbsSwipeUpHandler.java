e255e3fcaa71f8c05714e01c6376cb605367e6d7
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-03 21:34:02 +0800

    Launcher3: Fix handling of input consumer when swiping recents
    * this change was skipped on patch 64d51161e58a77e838a3368f9d85e042c1983074, before this CL, we were using input consumer handling meant for live tile feature which caused input consumer getting destroyed at the wrong time causing touch issues and recents view getting stucked when swiping up overview tasks.
    
    fixes: touch issues when swiping to home screen, swiping up overview tasks and gestures being stucked
    
    Change-Id: I290813090062ff24102aa4d6b55f025af64ed3ff
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -2420,11 +2420,9 @@ public abstract class AbsSwipeUpHandler<
     }
 
     private void invalidateHandler() {
-        if (!mContainerInterface.isInLiveTileMode() || mGestureState.getEndTarget() != RECENTS) {
-            mInputConsumerProxy.destroy();
-            mTaskAnimationManager.setLiveTileCleanUpHandler(null);
-        }
         mInputConsumerProxy.unregisterOnTouchDownCallback();
+        mInputConsumerProxy.destroy();
+        mTaskAnimationManager.setLiveTileCleanUpHandler(null);
         endRunningWindowAnim(false /* cancel */);
 
         if (mGestureEndCallback != null) {
@@ -2635,11 +2633,6 @@ public abstract class AbsSwipeUpHandler<
         }
         endLauncherTransitionController();
         mRecentsView.onSwipeUpAnimationSuccess();
-        mTaskAnimationManager.setLiveTileCleanUpHandler(() -> {
-            mRecentsView.cleanupRemoteTargets();
-            mInputConsumerProxy.destroy();
-        });
-        mTaskAnimationManager.enableLiveTileRestartListener();
 
         SystemUiProxy.INSTANCE.get(mContext).onOverviewShown(false, TAG);
         doLogGesture(RECENTS, mRecentsView.getCurrentPageTaskView());
