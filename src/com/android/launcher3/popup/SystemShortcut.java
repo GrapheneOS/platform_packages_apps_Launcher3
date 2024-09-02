7076e8932c2bad344821bcfd9d164b9da19a9feb
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-09-02 13:38:56 +0300

    add base class for Storage and Contact Scopes links

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -11,6 +11,7 @@ import static com.android.launcher3.widget.picker.model.data.WidgetPickerDataUti
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.GosPackageState;
 import android.content.pm.ShortcutInfo;
 import android.graphics.Rect;
 import android.os.Process;
@@ -20,12 +21,14 @@ import android.view.View;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.window.SplashScreen;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.android.launcher3.AbstractFloatingView;
 import com.android.launcher3.AbstractFloatingViewHelper;
+import com.android.launcher3.BaseActivity;
 import com.android.launcher3.Flags;
 import com.android.launcher3.LauncherSettings;
 import com.android.launcher3.R;
@@ -208,6 +211,38 @@ public abstract class SystemShortcut<T extends ActivityContext> extends ItemInfo
                 this.nodeId = nodeId;
             }
         }
+    }
+
+    abstract static class ScopesShortcut<T extends ActivityContext> extends SystemShortcut<T> {
+
+        protected String targetPackage;
+
+        private ScopesShortcut(int icon, int label, T target, ItemInfo itemInfo, View originalView) {
+            super(icon, label, target, itemInfo, originalView);
+            targetPackage = itemInfo.getTargetPackage();
+        }
+
+        protected static boolean hasGosPackageStateFlag(ItemInfo itemInfo, int flag) {
+            String pkg = itemInfo.getTargetPackage();
+            if (pkg == null) {
+                return false;
+            }
+            return GosPackageState.get(pkg, itemInfo.user).hasFlag(flag);
+        }
+
+        @Override
+        public void onClick(View v) {
+            dismissTaskMenuView();
+
+            Intent intent = getIntent(targetPackage);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            var opts = android.app.ActivityOptions.makeBasic()
+                    .setSplashScreenStyle(SplashScreen.SPLASH_SCREEN_STYLE_SOLID_COLOR)
+                    .toBundle();
+            v.getContext().startActivityAsUser(intent, opts, mItemInfo.user);
+        }
+
+        protected abstract Intent getIntent(String targetPkg);
     }
 
     public static final Factory<ActivityContext> PRIVATE_PROFILE_INSTALL =
