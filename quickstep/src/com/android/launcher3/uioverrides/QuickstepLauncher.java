ba8d79f381cd33aa73323ea0b80e6e677d3b9d92
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-16 22:08:28 +0300

    remove "Install in private" shortcut
    It opens the first party app source, which is not useful currently on GrapheneOS.

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -515,9 +515,6 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
             shortcuts.add(REMOVE);
         }
         shortcuts.add(DONT_SUGGEST_APP);
-        if (Flags.enablePrivateSpaceInstallShortcut()) {
-            shortcuts.add(PRIVATE_PROFILE_INSTALL);
-        }
         if (Flags.enablePrivateSpace()) {
             shortcuts.add(UNINSTALL_APP);
         }
