e8d3850417a3828facc18810f98d15a27dd83eb5
Frederico Silva <fredericojssilva@gmail.com>
2025-01-28 23:43:17 +0000

    Launcher3: Implement auto open keyboard in drawer setting
    Add setting to always show keyboard when swiping up on your home screen
    
    Change-Id: Ib2cdbcf23d5b46dc44a1641215f20c1ae20c4731
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Guidix <guidixm@gmail.com>
    Co-Authored-By: Michael Bestas <mkbestas@lineageos.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -22,6 +22,7 @@ import static com.android.launcher3.GridType.GRID_TYPE_NON_ONE_GRID;
 import static com.android.launcher3.GridType.GRID_TYPE_ONE_GRID;
 import static com.android.launcher3.LauncherPrefs.ALLAPPS_THEMED_ICONS;
 import static com.android.launcher3.LauncherPrefs.DB_FILE;
+import static com.android.launcher3.LauncherPrefs.DRAWER_OPEN_KEYBOARD;
 import static com.android.launcher3.LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE;
 import static com.android.launcher3.LauncherPrefs.FIXED_LANDSCAPE_MODE;
 import static com.android.launcher3.LauncherPrefs.FONT_SIZE;
@@ -327,7 +328,8 @@ public class InvariantDeviceProfile {
                     ICON_SIZE.getSharedPrefKey().equals(key) ||
                     FONT_SIZE.getSharedPrefKey().equals(key) ||
                     ENABLE_TWOLINE_ALLAPPS_TOGGLE.getSharedPrefKey().equals(key) ||
-                    ROW_HEIGHT.getSharedPrefKey().equals(key)) {
+                    ROW_HEIGHT.getSharedPrefKey().equals(key) ||
+                    DRAWER_OPEN_KEYBOARD.getSharedPrefKey().equals(key)) {
                 onConfigChanged();
             }
         };
