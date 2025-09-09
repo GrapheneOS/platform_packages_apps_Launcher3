cf6e0ad2edfeb25766d3aacf2a9278f66e30bcdc
LuK1337 <priv.luk@gmail.com>
2025-09-09 09:36:28 +0200

    Launcher3: Use HiddenAppsFilter for all apps list
    Change-Id: I1e79577da115f718a1a812acb5fe918d7bdb5fdd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/model/AllAppsList.java
+++ src/com/android/launcher3/model/AllAppsList.java
@@ -37,6 +37,7 @@ import com.android.launcher3.dagger.ApplicationContext;
 import com.android.launcher3.dagger.LauncherAppSingleton;
 import com.android.launcher3.icons.IconCache;
 import com.android.launcher3.lineage.trust.AppLockHelper;
+import com.android.launcher3.lineage.trust.HiddenAppsFilter;
 import com.android.launcher3.model.data.AppInfo;
 import com.android.launcher3.model.data.AppsListData;
 import com.android.launcher3.model.data.ItemInfo;
@@ -103,7 +104,7 @@ public class AllAppsList {
     @Inject
     public AllAppsList(@NonNull IconCache iconCache,
             @ApplicationContext Context context,
-            @NonNull AppFilter appFilter,
+            @NonNull HiddenAppsFilter appFilter,
             @NonNull Provider<AppsListRepository> repositoryProvider) {
         mIconCache = iconCache;
         mAppFilter = appFilter;
