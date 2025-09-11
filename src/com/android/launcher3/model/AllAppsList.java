dbb96ea1a9283448cfcb5ae7b4a466479654466d
Pranav Vashi <neobuddy89@gmail.com>
2025-09-12 01:08:10 +0530

    Launcher3: Drop explicit applock hidden list check in AllAppsList
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/model/AllAppsList.java
+++ src/com/android/launcher3/model/AllAppsList.java
@@ -33,10 +33,8 @@ import androidx.annotation.NonNull;
 import com.android.launcher3.AppFilter;
 import com.android.launcher3.Flags;
 import com.android.launcher3.compat.AlphabeticIndexCompat;
-import com.android.launcher3.dagger.ApplicationContext;
 import com.android.launcher3.dagger.LauncherAppSingleton;
 import com.android.launcher3.icons.IconCache;
-import com.android.launcher3.lineage.trust.AppLockHelper;
 import com.android.launcher3.lineage.trust.HiddenAppsFilter;
 import com.android.launcher3.model.data.AppInfo;
 import com.android.launcher3.model.data.AppsListData;
@@ -85,7 +83,6 @@ public class AllAppsList {
     @NonNull private final Provider<AppsListRepository> mRepo;
 
     private boolean mDataChanged = false;
-    private AppLockHelper mAppLockHelper;
 
     private AlphabeticIndexCompat mIndex;
 
@@ -103,13 +100,11 @@ public class AllAppsList {
      */
     @Inject
     public AllAppsList(@NonNull IconCache iconCache,
-            @ApplicationContext Context context,
             @NonNull HiddenAppsFilter appFilter,
             @NonNull Provider<AppsListRepository> repositoryProvider) {
         mIconCache = iconCache;
         mAppFilter = appFilter;
         mRepo = repositoryProvider;
-        mAppLockHelper = AppLockHelper.getInstance(context);
         mIndex = new AlphabeticIndexCompat(LocaleList.getDefault());
     }
 
@@ -161,9 +156,6 @@ public class AllAppsList {
     }
 
     public void add(AppInfo info, LauncherActivityInfo activityInfo, boolean loadIcon) {
-        if (mAppLockHelper != null && mAppLockHelper.isPackageHidden(info.getTargetPackage())) {
-            return;
-        }
         if (!mAppFilter.shouldShowApp(info.componentName)) {
             return;
         }
