4633a995935a723a1168691ccc984fb3d33768c0
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-11-16 22:38:28 +0200

    add workaround for prepareForAtomicAnimation() NPE crash

--- quickstep/src/com/android/launcher3/uioverrides/states/QuickstepAtomicAnimationFactory.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/QuickstepAtomicAnimationFactory.java
@@ -161,7 +161,7 @@ public class QuickstepAtomicAnimationFactory extends
             if (isWorkspaceVisible) {
                 CellLayout currentChild = (CellLayout) workspace.getChildAt(
                         workspace.getCurrentPage());
-                isWorkspaceVisible = currentChild.getVisibility() == VISIBLE
+                isWorkspaceVisible = currentChild != null && currentChild.getVisibility() == VISIBLE
                         && currentChild.getShortcutsAndWidgets().getAlpha() > 0;
             }
             if (!isWorkspaceVisible) {
