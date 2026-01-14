VoltageOS-staging/packages_apps_Launcher3

Launcher3: dark text on appdrawer toggle
Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/android/launcher3/settings/SettingsAppDrawer.java
@@ -115,7 +115,8 @@ public class SettingsAppDrawer extends CollapsingToolbarBaseActivity
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (LauncherPrefs.DRAWER_SEARCH.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.DRAWER_SCROLLBAR.getSharedPrefKey().equals(key) ||
-                LauncherPrefs.KEY_FORCE_ALL_APPS_ON_BOTTOM_SHEET.getSharedPrefKey().equals(key)) {
+                LauncherPrefs.KEY_FORCE_ALL_APPS_ON_BOTTOM_SHEET.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.ALL_APPS_DARK_TEXT.getSharedPrefKey().equals(key)) {
             LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
     }

GrapheneOS/platform_packages_apps_Launcher3

src/com/android/launcher3/settings/SettingsAppDrawer.java
@@ -114,7 +114,8 @@ public class SettingsAppDrawer extends CollapsingToolbarBaseActivity
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (LauncherPrefs.DRAWER_SEARCH.getSharedPrefKey().equals(key) ||
-                LauncherPrefs.DRAWER_SCROLLBAR.getSharedPrefKey().equals(key)) {
+                LauncherPrefs.DRAWER_SCROLLBAR.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.ALL_APPS_DARK_TEXT.getSharedPrefKey().equals(key)) {
             LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
     }