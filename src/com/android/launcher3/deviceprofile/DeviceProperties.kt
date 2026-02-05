141d516119e27ddab2e5e24f8633e243feaefa0e
finish0314 <finish21190@gmail.com>
2026-02-05 21:35:37 +0800

    Launcher3: Fix crash when first boot
    Change-Id: I71ea4acf0f95d90fc549ba07faf06e2f23ee2a86
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/deviceprofile/DeviceProperties.kt
+++ src/com/android/launcher3/deviceprofile/DeviceProperties.kt
@@ -17,6 +17,7 @@
 package com.android.launcher3.deviceprofile
 
 import android.content.Context
+import android.os.UserManager
 import com.android.launcher3.LauncherPrefs
 import com.android.launcher3.util.DisplayController
 import com.android.launcher3.util.WindowBounds
@@ -91,10 +92,15 @@ data class DeviceProperties(
             isExternalDisplay: Boolean,
             isGestureMode: Boolean,
         ): DeviceProperties {
+            val isUserUnlocked = 
+            context.getSystemService(UserManager::class.java).isUserUnlocked
             val systemIsTablet = info.isTablet(windowBounds)
             val isRotationAllowed = info.isRotationAllowed()
-            val prefs = LauncherPrefs.get(context)
-            val forceTabletStyle = prefs.get(LauncherPrefs.TABLET_OVERVIEW_STYLE)
+            val forceTabletStyle = if (isUserUnlocked) { 
+                LauncherPrefs.get(context).get(LauncherPrefs.TABLET_OVERVIEW_STYLE) 
+            } else {
+                false 
+            }
             val isTablet = systemIsTablet || forceTabletStyle
 
             val windowX = windowBounds.bounds.left
