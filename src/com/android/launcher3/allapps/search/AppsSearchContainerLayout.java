0a0a855d71de17b7245070c1c005fec0e425e540
inthewaves <inthewaves@pm.me>
2026-04-16 15:28:27 -0700

    Revert "allapps: hide A-Z apps list when search field is focused"
    This reverts commit 41a7fe2bc111eac05548da2958707c2784298ab6.

--- src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
+++ src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -46,7 +46,6 @@ import com.android.launcher3.search.SearchCallback;
 import com.android.launcher3.views.ActivityContext;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Layout to contain the All-apps search UI.
@@ -98,14 +97,6 @@ public class AppsSearchContainerLayout extends ExtendedEditText
 
             @Override
             public void afterTextChanged(Editable s) {
-            }
-        });
-
-        addOnFocusChangeListener((v, hasFocus) -> {
-            if (hasFocus && !mIsSearchSessionActive) {
-                mIsSearchSessionActive = true;
-                // non-null list to trigger animateToSearchState
-                mAppsView.setSearchResults(Collections.emptyList());
             }
         });
 
