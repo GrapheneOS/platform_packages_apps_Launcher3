84dcd2b496a5224bb8a3322032c1e65cd6493b09
0xALUCARD <0xALUCARD@protonmail.com>
2026-09-10 04:04:28 +0200

    move hide nav hint to its own alpha channel

--- quickstep/src/com/android/launcher3/taskbar/StashedHandleViewController.java
+++ quickstep/src/com/android/launcher3/taskbar/StashedHandleViewController.java
@@ -71,7 +71,8 @@ public class StashedHandleViewController implements TaskbarControllers.LoggableT
     public static final int ALPHA_INDEX_NUDGED = 4;
     public static final int ALPHA_INDEX_ALL_SET_TRANSITION = 5;
     public static final int ALPHA_INDEX_CUEBAR_HIDDEN = 6;
-    private static final int NUM_ALPHA_CHANNELS = 7;
+    public static final int ALPHA_INDEX_HIDE_NAV_HANDLE = 7;
+    private static final int NUM_ALPHA_CHANNELS = 8;
 
     // Values for long press animations, picked to most closely match navbar spec.
     private static final float SCALE_TOUCH_ANIMATION_SHRINK = 0.85f;
@@ -153,7 +154,9 @@ public class StashedHandleViewController implements TaskbarControllers.LoggableT
         mStashedHandleView.getLayoutParams().height = mTaskbarSize + taskbarBottomMargin;
 
         mTaskbarStashedHandleAlpha.get(ALPHA_INDEX_STASHED).setValue(
-                activity.isPhoneGestureNavMode() && !activity.isHideNavHandle() ? 1 : 0);
+                activity.isPhoneGestureNavMode() ? 1 : 0);
+        mTaskbarStashedHandleAlpha.get(ALPHA_INDEX_HIDE_NAV_HANDLE).setValue(
+                activity.isHideNavHandle() ? 0 : 1);
         mTaskbarStashedHandleHintScale.updateValue(1f);
 
         final int stashedTaskbarHeight = mControllers.taskbarStashController.getStashedHeight();
